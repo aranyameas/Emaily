d4dcaf7ef4b64df3e2455c106c107e24b494caf6
Aranya <2839645+aranyameas@users.noreply.github.com>
2019-02-08 17:13:19 -0800

    added mongo database feature to save users

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User';)
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -14,3 +19,4 @@ app.listen(PORT);
 //heroku open
 //console.developers.google.com
 //passport js
+//mlab
